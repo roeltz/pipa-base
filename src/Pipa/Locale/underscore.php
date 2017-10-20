7ab91f4fe2ec49c080a042b0d30d257b3a4d87b2
Leonardo Rothe Tagliafico <leonardo.rothe@shiftactive.com>
2017-10-20 18:13:29 -0500

    Define __() conditionally

--- src/Pipa/Locale/underscore.php
+++ src/Pipa/Locale/underscore.php
@@ -1,11 +1,21 @@
 <?php
 
-use Pipa\Locale\Locale;
+namespace Pipa\Locale {
 
-function __($message, $parameters = array(), $domain = "default") {
-	if ($locale = Locale::get()) {
-		return \Pipa\fill($locale->translate($message, $domain), $parameters);
-	} else {
-		return \Pipa\fill($message, $parameters);
+	function translate($message, $parameters = array(), $domain = "default") {
+		if ($locale = Locale::get()) {
+			return \Pipa\fill($locale->translate($message, $domain), $parameters);
+		} else {
+			return \Pipa\fill($message, $parameters);
+		}
+	}
+}
+
+namespace {
+
+	if (!function_exists("__")) {
+		function __() {
+			call_user_func_array("Pipa\Locale\translate", func_get_args());
+		}
 	}
 }
